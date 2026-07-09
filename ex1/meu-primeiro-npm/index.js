f174e1d571597cba802218672092d6e427d0be47
NATHÁLIA LETICIA RODRIGUES <nathalia_rodrigue150@estudante.sesisenai.org.br>
2026-07-09 20:27:32 -0300

    feat:A

---
+++ ex1/meu-primeiro-npm/index.js
@@ -0,0 +1,4 @@
+import prompt from 'prompt-sync';
+
+const promptFn = prompt();
+console.log("Projeto NPM funcionando!");
---
+++ ex1/meu-terceiro-npm/index.js
@@ -0,0 +1,5 @@
+import sillyname from 'sillyname';
+
+console.log("nome:", sillyname());
+console.log("nome:", sillyname());
+console.log("nome:", sillyname());
